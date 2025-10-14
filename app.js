d5c1339e4ddc5b076a875315d0c7b46bd32ba42a
konradkelly <konradky@gmail.com>
2025-10-14 14:48:48 -0700

    Update port to 3002

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${import.meta.dirname}/views/home.html`);
+app.get("/", (req, res) => {
+	res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-}); 
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
