41115854e375ce1057805d5c6744090290def3f2
konradkelly <konradky@gmail.com>
2025-11-03 21:38:45 -0800

    implement admin route with a page that displays DataTables and a confirmation page that shows an order summary

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 import express from "express";
 
 const app = express();
-const PORT = 3002;
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const orders = [];
+
+const PORT = 3002;
 
 app.get("/", (req, res) => {
-	res.sendFile(`${import.meta.dirname}/views/home.html`);
+	res.render("home");
+});
+
+app.post("/submit-order", (req, res) => {
+	const order = {
+		name: req.body.name,
+		email: req.body.email,
+		flavor: req.body.flavor,
+		cone: req.body.cone,
+		toppings: [].concat(req.body.toppings || []), //prevents type error when a String is passed in instead of an Array
+		comment: req.body.comment,
+		timestamp: new Date(),
+	};
+	orders.push(order);
+	console.log(orders);
+	res.render("confirmation", { order });
+});
+
+app.get("/admin", (req, res) => {
+	res.render("admin", { orders });
 });
 
 app.listen(PORT, () => {
